6f5e5931564d85e6713359a976ebe525b341f6ff
jxk <v_jxkjiang@tencent.com>
2024-04-03 18:06:59 +0800

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+import { Config } from "tailwindcss";
+const tailwindConfig: Config = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+export default tailwindConfig;
